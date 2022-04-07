3577951db04f5d13d279503949116914bb95e715
doraly <doraly9110@hotmail.com>
2022-04-07 10:23:36 -0500

    adding about component

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#023047] text-white">
+      <div className="flex justify-center items-center w-full h-full">
+        <div>
+            <div>
+                <h1>ABOUT</h1>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
